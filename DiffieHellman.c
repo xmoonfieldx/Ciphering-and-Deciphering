890b2197baf14342730180bc90e3e1b3022148fd
Manjeera Reddy <61614013+xmoonfieldx@users.noreply.github.com>
2022-08-04 15:57:55 +0530

    Update DiffieHellman.c

--- DiffieHellman.c
+++ DiffieHellman.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 int power(int a, int b, int q)
 {
     if(b==1)
         return a;
-    else
-        return computepower(a,b)%q;
-        
+    return computepower(a,b)%q;
+    
 }
 int computepower(int a, int b)
 {
@@ -16,27 +17,19 @@ int computepower(int a, int b)
 }
 int main()
 {
-    int Ya, Yb, Ka, Kb, A, q, a, b;
-    printf("Primitive root(A): ");
+    int A, q, a, b, Ya, Yb, Ka, Kb;
+    printf("Primite Root(A): ");
     scanf("%d",&A);
     printf("Prime number(q): ");
     scanf("%d",&q);
-    printf("A's private key: ");
+    printf("Alice's private key: ");
     scanf("%d",&a);
-    printf("B's private key: ");
+    printf("Bob's private key: ");
     scanf("%d",&b);
-    
-    printf("\n\n A's public Key: ");
     Ya=power(A,a,q);
-    printf("%d",Ya);
-    
-    printf("\nB's public key: ");
     Yb=power(A,b,q);
-    printf("%d",Yb);
-    
-    printf("\nShared Keys\n");
-    Ka=power(Ya,b,q);
-    Kb=power(Yb,a,q);
-    printf("Ka: %d, Kb: %d",Ka,Kb);
-    
+    printf("Public Keys\nAlice: %d Bob: %d\n",Ya,Yb);
+    Ka=power(Yb,a,q);
+    Kb=power(Ya,b,q);
+    printf("Shared Keys\nAlice: %d Bob:%d\n",Ka,Kb);
 }
